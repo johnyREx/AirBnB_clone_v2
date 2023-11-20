1b294895226385e5d372f95e340026adca206397
johnyREx <johnkwofie99@icloud.com>
2023-11-20 05:00:08 -0800

    bug fixing from last night

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- This Prepares the MySQL server for this project
+-- This  Script Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'loacalhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
