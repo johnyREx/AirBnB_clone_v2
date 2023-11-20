f01a76a7483206d709ab53aa3cef0c0aebef2017
johnyREx <johnkwofie99@icloud.com>
2023-11-20 10:40:15 -0800

    updated all the test models

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """ """
+import os
+
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """The tests for the Amenity model."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Test the type of name."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+                type(new.name),
+                str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
