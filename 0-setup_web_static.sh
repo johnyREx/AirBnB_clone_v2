ccba372bb2c1dcd50fb3f2c0811d90c21d3f0bb7
johnyREx <johnkwofie99@icloud.com>
2023-12-06 07:48:02 -0800

    task 0 fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,21 @@
 #!/usr/bin/env bash
 # This script sets up web servers for the deployment of web_static.
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-	sudo apt-get -y update
-	sudo apt-get -y install nginx
-fi
-
-# Create nevcessary folders if they dont exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# create a fake HTML file
-echo echo "<html><head></head><body>Test Content</body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link
-sudo chown -R ubuntu:ubuntu /data/
-
-# Give ownership to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_content="
-server {
-    listen 80;
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/;
-    }
-
-error_page 404 /404.html;
-    location /404 {
-        alias /custom_404.html;
-        internal;
-    }
-}
-"
-
-echo "$config_content" | sudo tee /etc/nginx/sites-available/default
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 
 # Restart Nginx
 sudo service nginx restart
